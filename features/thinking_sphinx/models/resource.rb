a0adb40f282d8977bacf69dfba1b17da26133332
Ben Hutton <benhutton@gmail.com>
2011-06-01 14:07:06 -0400

    The first way might be the best for now

--- features/thinking_sphinx/models/resource.rb
+++ features/thinking_sphinx/models/resource.rb
@@ -4,7 +4,18 @@ class Resource < ActiveRecord::Base
   define_index do
     indexes translations(:name)
 
-    has translations.locale, :as => 'locales'
     has "GROUP_CONCAT(DISTINCT CRC32(IFNULL(`resource_translations`.`locale`, '0')) SEPARATOR ' ')", :as => 'locale', :type => :multi
   end
+
+  # define_index 'resource_en' do
+    # indexes translations(:name)
+
+    # where "resource_translations.locale='en'"
+  # end
+
+  # define_index 'resource_es' do
+    # indexes translations(:name)
+
+    # where "resource_translations.locale='es'"
+  # end
 end
